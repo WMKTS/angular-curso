91cb9c674b28eb48a1e6df8cd5b2cb07ada891cb
Diego Henrique Franco <diegohfranco@gmail.com>
2019-01-17 16:34:52 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
-export class InMemoryDataBase implements InMemoryDataBase{
+import { Category} from "./pages/categories/shared/category.model";
+
+export class InMemoryDataBase implements InMemoryDbService{
     createDb(){
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: "Cão", description: "Cães de todas as raças" },
             { id: 2, name: "Felinos", description: "Gatos de todas as Raças" },
             { id: 3, name: "Aves", description: "Aves de todas as raças" },
             { id: 4, name: "Répteis", description: "Calangos, Cobras, Camaleões" },
-            { id: 5, name: "Diversos", description: "Animais Diversos" }
+            { id: 5, name: "Diversos", description: "Animais Diversos" }            
         ];
         return {categories}
     }
