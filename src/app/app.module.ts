8e1dd59486ea977e77e066d8da2eedefe906df9a
Diego Henrique Franco <diegohfranco@gmail.com>
2019-01-17 17:35:33 -0300

    intercepting http requests with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//REMOVER LINHAS ABAIXO QUANDO FOR UTILIZAR API REAL
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataBase } from "./in-memory-database";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //REMOVER LINHA ABAIXO QUANDO FOR UTILIZAR API REAL
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
   bootstrap: [AppComponent]
